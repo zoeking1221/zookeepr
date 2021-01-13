c2cc71600ad7ff859a6ffc999e47463f5cb37e18
Zoe King <zoeking@Zoes-MacBook-Pro.local>
2021-01-12 20:30:25 -0500

    made PORT a environmental variable

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const { animals } = require('./data/animals.json');
 
 const express = require('express');
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 function filterByQuery(query, animalsArray) {
@@ -50,6 +51,6 @@ function filterByQuery(query, animalsArray) {
     res.json(results);
   });
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 });
